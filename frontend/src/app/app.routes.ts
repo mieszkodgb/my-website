fe382561c6a0bb0c6af7874daeeb5a97b6c11f52
Mieszko Di Gregorio Botek <mieszkodgb@gmail.com>
2024-12-28 14:55:45 +0100

    add home page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'skills', component: SkillsComponent},
+  {path: 'exp', component: ExperienceComponent},
+  {path: 'edu', component: EducationComponent},
+  {path: 'contact', component: ContactComponent},
+];
